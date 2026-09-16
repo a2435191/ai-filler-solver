33618070dec1b34b4704b82c99c66e793ca915f9
Will Bradley <68962413+a2435191@users.noreply.github.com>
2026-09-16 09:41:53 -0400

    Add a real game

--- bin/main.ml
+++ bin/main.ml
@@ -2,13 +2,13 @@ open Filler
 open Board
 
 let string =
-  {| 🟨⬛🟪🟩🟥🟦🟦🟦
-     🟥🟦🟥🟦🟩🟪🟥🟥
-     🟨🟥🟩⬛🟪⬛⬛⬛
-     🟥🟨🟦🟪🟨🟩⬛🟪
-     🟥🟦⬛⬛⬛🟪🟩🟦
-     🟩⬛🟩🟦🟪🟨🟪⬛
-     ⬛🟪🟥🟩🟩🟨🟥🟨 |}
+  {|🟪🟥🟦⬛🟪🟦🟥🟥                   
+    🟦🟨🟩🟨🟥⬛🟦⬛
+    🟥🟩🟦🟥⬛🟩⬛🟦
+    🟪🟥🟩🟦🟪🟥🟩⬛
+    🟩🟦🟪⬛🟨⬛🟨🟪
+    🟪⬛🟦🟩🟥🟦🟪🟦
+    ⬛🟨🟩🟥🟦🟥🟩🟨 |}
 
 let board = parse string
 let () = print board
@@ -18,5 +18,5 @@ let () = print_newline ()
 let () = print board'
 let () = print_int (region_size board' Opp)
 let () = print_newline () *)
-let move, score = Minimax.minimax board
+let move, score = Minimax.minimax board ~player:Us ~max_depth:12
 let () = Printf.printf "Best move: %s. Score: %f\n" (Color.to_square move) score
