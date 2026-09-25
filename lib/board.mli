c55a585b962f71d98d3900c9aee53cea9331eea3
Will Bradley <68962413+a2435191@users.noreply.github.com>
2026-09-25 17:48:44 -0400

    Add yet more comments

--- lib/board.mli
+++ lib/board.mli
@@ -4,9 +4,16 @@ type t
 (* Constants *)
 
 val height : int
+(** [7] *)
+
 val width : int
+(** [8] *)
+
 val total_squares : int
+(** [width * height] *)
+
 val squares_to_tie : int
+(** [total_squares / 2] *)
 
 (* Basic helper functions *)
 
