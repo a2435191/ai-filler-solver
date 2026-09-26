780911cbc57145c54b877d475f91902aec5198da
Will Bradley <68962413+a2435191@users.noreply.github.com>
2026-09-25 20:39:59 -0400

    Modify driver program

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,6 @@
 open Filler
 open Board
+open Head2head
 
 let string =
   {|🟪🟥🟦⬛🟪🟦🟥🟥                   
@@ -14,9 +15,22 @@ let board = parse string
 let () = print board
 let () = print_newline ()
 
+let strategies =
+  [
+    (* minimax 0;
+    minimax 1;
+    minimax 5;
+    minimax 10; *)
+    random;
+    greedy Minimax.heuristic "default";
+  ]
+
+let results = head_to_head ~trials:10 ~boards:(fun _ -> board) strategies
+let () = print_results results
+
 (* let board' = move board Color.Red Opp
 let () = print board'
 let () = print_int (region_size board' Opp)
 let () = print_newline () *)
-let move, score = Minimax.minimax board ~player:Us ~max_depth:12
-let () = Printf.printf "Best move: %s. Score: %f\n" (Color.to_square move) score
+(* let move, score = Minimax.minimax board ~player:Us ~max_depth:12
+let () = Printf.printf "Best move: %s. Score: %f\n" (Color.to_square move) score *)
